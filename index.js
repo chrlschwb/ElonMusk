ed62dea5082c9566b136765e8b3ff23965e69463
create <chrlschwb77@gmail.com>
2023-03-07 13:53:47 -0800

    test

--- index.js
+++ index.js
@@ -127,4 +127,14 @@ const startServer = async () => {
   });
 };
 
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+        //Set Static folder
+        app.use(express.static('client/build'));
+        
+        app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        })
+
+}
 startServer();
